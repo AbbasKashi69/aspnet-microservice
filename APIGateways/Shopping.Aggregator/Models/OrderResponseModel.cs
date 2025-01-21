cc72b425e57da4cdd1f3e085fd89c0987776c10c
AbbasKashi69 <AbbasKashi69@gmail.com>
2025-01-21 13:54:23 +0330

    پیاده سازی shopping aggregator

---
+++ APIGateways/Shopping.Aggregator/Models/OrderResponseModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Shopping.Aggregator.Models
+{
+    public class OrderResponseModel
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string? Email { get; set; }
+        public string City { get; set; }
+
+        public string BankName { get; set; }
+        public int PaymentMethod { get; set; }
+    }
+}
---
+++ APIGateways/Shopping.Aggregator/ShoppingMinimalApi.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shopping.Aggregator.Models;
+using Shopping.Aggregator.Services;
+
+namespace Shopping.Aggregator
+{
+    public static class ShoppingMinimalApi
+    {
+        public static void Config(WebApplication app)
+        {
+            app.MapGet("/getshopping",
+                async (ICatalogService catalogService,
+                IBasketService basketService,
+                IOrderService orderService,
+                [FromQuery] string userName) =>
+            {
+                var basket = await basketService.GetBasket(userName);
+
+                foreach (var item in basket.Items)
+                {
+                    var product = await catalogService.GetCatalog(item.ProductId);
+
+                    item.ProductName = product.Name;
+                    item.Category = product.Category;
+                    item.Summary = product.Summary;
+                    item.Description = product.Description;
+                    item.ImageFile = product.ImageFile;
+                }
+
+                var orders = await orderService.GetOrderByUserName(userName);
+
+                var shoppingModel = new ShoppingModel
+                {
+                    UserName = userName,
+                    BasketWithProduct = basket,
+                    Orders = orders
+                };
+
+                return shoppingModel;
+            })
+            .WithName("getshopping")
+            .WithOpenApi();
+
+        }
+    }
+}
